6831e27dd32bd2feb5ae3f7ab8533e30755969a8
RanjithSatla <ranjithsatla327@gmail.com>
2022-03-09 18:59:21 +0530

    Day 1 : Create, Get, Update, Delete Todo's

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const res = require("express/lib/response");
+const { middlewares } = require("./middlewares");
 const app = express();
 
 const db = require("./config/db");
 
+const todoRoute = require("./routes/todoRoute");
+
+//middlewares
+app.use(middlewares);
+
+app.use("/", todoRoute);
+
 const server = app.listen(process.env.PORT || 5000, () => {
   const port = server.address().port;
+
   console.log(`Express is working on port ${port}`);
 });
---
+++ routes/todoRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const Todo = require("../controllers/todo");
+//controllers
+
+const router = express.Router();
+
+// Day 1 Todo List
+router.post("/addTodo", Todo.createTodo);
+router.get("/getAll", Todo.getAllTodo);
+router.get("/:id", Todo.getOneTodo);
+router.put("/:id", Todo.updateTodo);
+router.delete("/:id", Todo.deleteTodo);
+
+module.exports = router;
